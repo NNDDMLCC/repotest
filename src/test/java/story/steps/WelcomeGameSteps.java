646f37227694d4e2a7a011b1bdcc48a758bc0844
cdcarloschacon <cdcarloschacon@gmail.com>
2013-12-14 14:55:09 -0500

    update

--- src/test/java/story/steps/WelcomeGameSteps.java
+++ src/test/java/story/steps/WelcomeGameSteps.java
@@ -7,35 +7,44 @@ import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.springframework.util.Assert;
 
+import story.pages.StartGamePage;
 import static org.junit.Assert.*;
 
 public class WelcomeGameSteps {
 
 	WebDriver webdriver;
+	StartGamePage startPage;
 
 	@BeforeScenario
 	public void setupWebDriver() {
-		//webdriver=new FirefoxDriver();
+		this.webdriver = new FirefoxDriver();
+		this.startPage = new StartGamePage(this.webdriver);
 	}
 	
+
 	@Given("The game has not started")
 	public void givenTheGameHasNotStarted() {
 	}
 	
+
 	@When("The challenger access the system")
 	public void whenTheChallengerAccessTheSystem() {
-		
+		this.startPage.open();
 	}
 	
 	
 	@Then("The challenger see a welcome message")
 	public void thenISeeAWelcomeMessage() {
-		System.out.println("then");
+		String msg = this.startPage.retrieveWelcomeMessage();
+		Assert.notNull(msg);
+		Assert.isTrue(!msg.isEmpty());
 	}
 	
+
 	@AfterScenario
 	public void closeWebDriver() {
-		//webdriver.close();
+		this.webdriver.close();
 	}
 }
