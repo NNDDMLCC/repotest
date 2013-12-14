8b0bb098c984b5f54e3b954599ac87ce8a3059af
cdcarloschacon <cdcarloschacon@gmail.com>
2013-12-14 14:55:46 -0500

    Initial Import.

---
+++ src/test/java/story/pages/StartGamePage.java
@@ -0,0 +1,23 @@
+package story.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class StartGamePage {
+
+	private WebDriver webDriver;
+	
+	public StartGamePage(WebDriver webDriver) {
+		super();
+		this.webDriver = webDriver;
+	}
+	
+	public void open() {
+		this.webDriver.get("http://localhost:8080/ahorcado");
+	}
+
+	public String retrieveWelcomeMessage() {
+		return this.webDriver.findElement(By.id("")).getText();
+	}
+	
+}
